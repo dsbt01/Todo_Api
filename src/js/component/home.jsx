c3d20596339bf65fe558528ca2d7a11040de097e
David Suarez <dsuarez001@att.net>
2022-11-02 18:25:45 -0400

    add is working

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -44,13 +44,37 @@ const Home = () => {
         label: inputValue,
       };
 
+	  console.log(newItem);
+	  
       globalList.push(newItem);
 
+	  console.log(globalList);
+
       //now push the value to the web api
+      fetch("https://assets.breatheco.de/apis/fake/todos/user/DSBT", {
+        method: "PUT",
+        body: JSON.stringify(globalList),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => {
+          console.log(resp.ok); // will be true if the response is successfull
+          console.log(resp.status); // the status code = 200 or code = 400 etc.
+          console.log(resp.text()); // will try return the exact result as string
+          return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+        })
+        .then((data) => {
+          //here is were your code should start after the fetch finishes
+          console.log(data); //this will print on the console the exact object received from the server
+          RefreshData();
+        })
+        .catch((error) => {
+          //error handling
+          console.log(error);
+        });
 
       setInputValue("");
-
-	  RefreshData();
     }
   }
 
@@ -88,7 +112,9 @@ const Home = () => {
       });
   }
 
-  useEffect(() => {RefreshData();}, []);
+  useEffect(() => {
+    RefreshData();
+  }, []);
 
   return (
     <div className="container">
